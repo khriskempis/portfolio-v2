0709bd2e1678a6c672eebca7649f8c0ffa7c2229
Khris Kempis <khriskempis@gmail.com>
2020-06-13 22:53:01 -0700

    initial commit

---
+++ resources/views/partials/hero.blade.php
@@ -0,0 +1,15 @@
+<div class="hero">
+    <h1 class="name">Hi, my name is Khris Kempis</h1>
+    <ul>
+        <li class="music-word">
+            <div class="iAm">I am a</div>
+            <div class="words-animate">
+                <div class="word teacher-word">teacher</div>
+                <div class="word musician-word">musician</div>
+                <div class="word developer-word">developer</div>
+            </div>
+        </li>
+    </ul>
+    <p>I love working on problems and can help you grow your product from start to finish.</p>
+    <a type="button" href="mailto:khriskempis@gmail.com">Let's Chat</a>
+</div>
